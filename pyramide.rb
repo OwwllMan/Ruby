e02db551e5d4880f76d4bcf95caf49a773ca4eb6
BigBiceps <39001414+OwwllMan@users.noreply.github.com>
2018-09-28 19:11:39 +0200

    Update pyramide.rb

--- pyramide.rb
+++ pyramide.rb
@@ -2,7 +2,12 @@ puts "Choisi un nombre "
 print "> "
 userNumber = gets.chomp.to_i
 n = 1
-while n <= userNumber
-  puts ("# " * n).rjust(userNumber * 2)
-  n += 1
+
+if userNumber > 25
+  puts "Pas plus de 25 étages ! "
+else 
+  while n <= userNumber
+    puts ("# " * n).rjust(userNumber * 2)
+    n += 1
+  end
 end
